94ed56b439ae021352e876af81aaf853d3838f49
Strapontin <antodu73@hotmail.fr>
2017-10-17 12:53:15 +0200

    Mise à jour

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
-  #mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'homepage#index'
+  localized do
 
-  post '/listings/contact', to: 'listings#contact', as: 'listing_contact'
+      devise_for :users
+      #mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-  #apipie
+      # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+      root 'homepage#index'
+      resources :listings
+
+      post '/listings/contact', to: 'listings#contact', as: 'listing_contact'
+
+      #apipie
+
+  end
+
+
 end
