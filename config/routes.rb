364b68e4c05dd48ecee0617f07e0aac80689636f
Titi <thibault.buathier@k-net.pro>
2017-10-18 13:58:04 +0200

    fix admin localized

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   localized do
     devise_for :users
     mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
