e4370709c72e9c2d1b9aec29c9731b71614d69b5
Titi <thibault.buathier@k-net.pro>
2017-10-23 09:50:53 +0200

    add migration lang pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018120333) do
+ActiveRecord::Schema.define(version: 20171023073354) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20171018120333) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "lang", default: "EN", null: false
   end
 
   create_table "roles", force: :cascade do |t|
