c8fc0a3ec6c2509b0ff655fb71bf77a6345603d8
Titi <thibault.buathier@k-net.pro>
2017-10-23 09:05:51 +0200

    fix default picture

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,14 @@
 class Listing < ApplicationRecord
+
+  DEFAULT_THUMBNAIL = 'http://media.istockphoto.com/photos/pig-picture-id120534386?k=6&m=120534386&s=612x612&w=0&h=nsy1ZpqPri6EfAaAOYgjLjpT5GjHGkgtEq0YTRiG6UU='
+
   mount_uploader :picture, PictureUploader
 
   belongs_to :user
   belongs_to :category
   has_many :comments
+
+  def thumbnail
+    picture.url ? picture : DEFAULT_THUMBNAIL
+  end
 end
